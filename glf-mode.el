512e555ce4578928b52a6c0b729002f5be111886
Christophe Mealares <cmealares@server.fake>
2013-05-15 22:18:06 +0200

    adding license

--- glf-mode.el
+++ glf-mode.el
@@ -6,7 +6,24 @@
 ;;     Christophe Mealares
 ;;     Laurent P
 ;;
+
+;;; License:
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 ;;; Main features:
+;;
 ;;    Syntax coloration
 ;;    Focus on a particular thread by hiding others
 ;;    Thread navigation:
@@ -16,8 +33,9 @@
 ;;    Indent nested scopes
 ;;    Goto xml trace file
 ;;    Goto source code
-;;
+
 ;;; Installation:
+;;
 ;;    Put this file on your load path.
 ;;    Then add the following lines to your Emacs initialization file:
 ;;
